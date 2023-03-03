500037421139e979801a692c21305635179ef11d
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-03-02 21:34:13 -0800

    ADD:Prime Factor Program is Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Primefactors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            primeFactors.factors();
+
+
+        }
+    }
+}
---
+++ primeFactors.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Primefactors
+{
+    public class primeFactors
+    {
+        public static void factors()
+        {
+            Console.WriteLine("Enter the prime factors: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=input;i++)
+            {
+                        Console.WriteLine(i);
+
+                for (int j = 1; j <= i; j++)
+                {
+                    int count = 0;
+                    if (i%j==0)
+                    {
+                        count++;
+                    }
+                    if (count == 2)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
